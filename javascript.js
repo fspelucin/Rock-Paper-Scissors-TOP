0f030649da2ed81bd172b2eb14288ed3627b3745
Pancho <fspeluin@gmail.com>
2026-01-03 16:15:46 -0500

    Creating pseudocode and functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
+let humanScore = 0;
+let computerScore = 0; 
+
+function getComputerChoice(){
+  //Get a random number from 1 to 3
+  //Declare computerChoide variable
+  //Switch random number to a rock-papper-scisors string value
+  //Put that value into computerChoice variable
+  //Return computerChoice variable
+
+  return computerChoice
+}
+
+function getHumanChoice(){
+  //Declare HumanChoice variable
+  //Use prompt method to get usser input
+  //Make usser input case insensitive
+  //Attach usser imput to getHumanChoice value
+  //Return humanChoice value
+  return humanChoice
+}
+
+function playRound()
 console.log("Hello World")
