ab5d56a35a04ad0ab4d31e9cf4af762eadc7d7b5
Pancho <fspelucin@gmail.com>
2026-01-20 17:58:04 -0500

    Finishing functions and clearing code from test console.logs and unused variable declarations

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0; 
-
 function getComputerChoice(){
   //Get a random number from 1 to 3
   let computerChoice; 
@@ -18,8 +15,6 @@ function getComputerChoice(){
   return computerChoice
 }
 
-let computerChoice = getComputerChoice();
-
 function getHumanChoice(){
   //Declare HumanChoice variable
   let HumanChoice; 
@@ -31,23 +26,65 @@ function getHumanChoice(){
   //Return humanChoice value
   return HumanChoice
 }
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanScore,computerScore){
   //Get humanChoice and computerChoice
   let humanChoice = getHumanChoice();
   let computerChoice = getComputerChoice();
+  let winner;
   //List of conditionals, to get the winner.
+  if (humanChoice==computerChoice){
+    console.log("This round is a DRAW!!!");
+    return
+  }
+  else {
+    if ((humanChoice=="rock")&&(computerChoice=="papper")){
+      winner = "Human";
+      humanScore = humanScore ++;
+    }
+    if((humanChoice=="papper")&&(computerChoice=="rock")){
+      winner = "Human";
+      humanScore = humanScore ++;
+    }
+    if ((humanChoice=="scissors")&&(computerChoice=="papper")){
+      winner = "Human" 
+      humanScore = humanScore ++;
+    }
+    else {
+      winner = "Computer"
+      computerScore = computerScore ++;
+    }
+  }
   //Console.log the winner of the round
+  console.log(`Human chose ${humanChoice}, Computer chose ${computerChoice}. The winner is ${winner}`);
   //Return humanScore and computerScore
+  if (winner == "Human"){
+    return humanScore
+  } 
+  else{
+    return computerScore
+  }
 }
 
-//function playgame(){
+function playgame(){
   //Create score variable
+  let humanScore = 0;
+  let computerScore = 0;
   //forloop 5 times 
-  //gethuman and computer choice and attach it to variable
-  //platround
-  //modify points
+  for (let i = 0; i < 5; i++) {
+    playRound(humanScore, computerScore);
+  }
   //console log winner of the game
-//
+  if (humanScore==computerScore){
+    console.log ("This game was a DRAW!!!");
+  }
+  else{
+    if(humanScore > computerScore){
+      console.log("The winner of this game is the HUMAN");
+    } 
+    else {
+      console.log("The winner of this game was the COMPUTER");      
+    }
+  }
+
+playgame(); 
